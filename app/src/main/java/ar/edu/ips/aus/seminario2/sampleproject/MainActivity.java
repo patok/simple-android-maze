4cffdee1009651512369cfc34fdd2f71cd94d8fa
Patricio Keilty <patricio.keilty@estudios-electricos.com>
2019-08-18 21:58:52 -0300

    Adding onClick listener for all buttons.

--- app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MainActivity.java
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/MainActivity.java
@@ -5,15 +5,34 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttonUp, buttonDown, buttonLeft, buttonRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maze);
 
+        buttonUp = findViewById(R.id.buttonUp);
+        buttonDown = findViewById(R.id.buttonDown);
+        buttonLeft = findViewById(R.id.buttonLeft);
+        buttonRight = findViewById(R.id.buttonRight);
+
+        buttonUp.setOnClickListener(this);
+        buttonDown.setOnClickListener(this);
+        buttonLeft.setOnClickListener(this);
+        buttonRight.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        // complete with event listener logic
+        Log.d("MAZE", String.format("View ID %d inside listener", v.getId()));
+    }
 }
