46eeb6b28bde4a72621988f9f3016525677df351
Patricio Keilty <patricio.keilty@estudios-electricos.com>
2021-11-01 18:31:28 -0300

    Added functionality to create new Game. UI incorporated input text field and button for new game.

--- app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/GameSelectionActivity.java
+++ app/src/main/java/ar/edu/ips/aus/seminario2/sampleproject/GameSelectionActivity.java
@@ -7,7 +7,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 public class GameSelectionActivity extends AppCompatActivity
         implements GameMetadataAdapter.ItemClickListener {
@@ -29,6 +36,22 @@ public class GameSelectionActivity extends AppCompatActivity
 
         adapter.setClickListener(this);
 
+        ImageButton newGameButton = findViewById(R.id.newGame);
+        EditText gameNameInput = findViewById(R.id.gameName);
+
+        newGameButton.setOnClickListener(view -> {
+            // push gameMetadata
+            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+            GameMetadata data = new GameMetadata();
+            String dataId = databaseReference.push().getKey();
+            data.setId(dataId);
+            data.setTitle(gameNameInput.getText().toString());
+            databaseReference.child(dataId).setValue(data);
+
+            Game.getInstance(GameSelectionActivity.this).setGameMetadata(data);
+
+            startNewGameActivity();
+        });
         Game.getInstance(this.getApplicationContext());
     }
 
@@ -38,6 +61,10 @@ public class GameSelectionActivity extends AppCompatActivity
         GameMetadata metadata = adapter.getData().get(position);
         Game.getInstance(this).setGameMetadata(metadata);
 
+        startNewGameActivity();
+    }
+
+    private void startNewGameActivity() {
         Intent activityIntent = new Intent(this, MazeBoardActivity.class);
         startActivity(activityIntent);
     }
